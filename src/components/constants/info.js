b61370ef03fa5f2315785e4a34d17666ea0e6a21
Caroline Oliveira <carolinevsboliveira@gmail.com>
2021-06-20 21:54:01 -0400

    Fixed info.

--- src/components/constants/info.js
+++ src/components/constants/info.js
@@ -1,9 +1,10 @@
 const Info = {
-    howToPlay: "2048 Cards is a solitaire game, in which you have to merge cards with the same number. When you have two identical cards, they will form a single card with double the number for which you will get coins or cash rewards. The target is to unlock the card 2048",
-    originalLogicCode: 'https://github.com/IvanVergiliev/2048-react', 
-    originalImageFiles: "https://www.behance.net/romaincousin", 
-    tutorialLink: "https://www.youtube.com/watch?v=ca0BEH2bBLs",
-    gitRepo: "https://github.com/carolinevsboliveira/2048"
-}
+  howToPlay:
+    "2048 Cards is a solitaire game, in which you have to merge cards with the same number. When you have two identical cards, they will form a single card with double the number for which you will get coins or cash rewards.",
+  originalLogicCode: "https://github.com/IvanVergiliev/2048-react",
+  originalImageFiles: "https://www.behance.net/romaincousin",
+  tutorialLink: "https://www.youtube.com/watch?v=ca0BEH2bBLs",
+  gitRepo: "https://github.com/carolinevsboliveira/2048",
+};
 
-export default Info
+export default Info;
